99354ce2829093bf2d202bc7c5fd93e4368b2b19
vvenetillo <venetillovictor@gmail.com>
2024-02-01 00:14:08 -0300

    update database

--- router/AppRouter.js
+++ router/AppRouter.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3001;
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  // password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   connectionLimit: 10,
 });
